c2faf8552e3aaa6c6bac72a62f4332b40993a872
kkig <kigoshi.kanazawa@gmail.com>
2021-01-28 18:49:39 +0900

    styled-components & react-query implemented

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import ReactDOM from 'react-dom';
+
+import Card from './components/Card';
+
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Pokédex/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders the correct content.', () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<Card />, root);
 });
